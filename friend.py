8a9304bfec10992748df1a3f4362a99550bb9f2c
Farzin Mohammadi <farzinlize@live.com>
2022-07-22 16:06:32 +0430

    update v2

--- friend.py
+++ friend.py
@@ -1,16 +1,8 @@
 import sys
 from socket import socket
-from common import IS_TXT, IS_FILE, HELP_MSG, read_protocol, send_protocol
+from common import IS_TXT, IS_FILE, HELP_MSG, communication_loop, read_protocol, send_protocol
 
 talky = socket()
 talky.connect((sys.argv[1], int(sys.argv[2])))
 
-while True:
-    command = input("[here] enter a command (h for help): ")
-    if   command in ['h', 'help']:print(HELP_MSG)
-    elif command in ['r', 'receive']:read_protocol(talky)
-    elif command in ['sf', 'send file']:send_protocol(talky, IS_FILE, input("(file name or address?)"))
-    elif command in ['sx', 'send txt']:send_protocol(talky, IS_TXT, input("(message?)"))
-    elif command in ['e', 'exit']:break
-
-print("communication's over see you next time")
+communication_loop(talky)
