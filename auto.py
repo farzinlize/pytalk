dd75f3d562ca3551bbcbc8e87e70918ffd332528
farzin <farzinlize@live.com>
2022-12-31 18:45:32 +0330

    fixing things

--- auto.py
+++ auto.py
@@ -1,13 +1,13 @@
 import sys
 from socket import socket, AF_INET, SOCK_STREAM
 from common import IS_FILE, read_protocol, send_protocol
-from config import AUTO_PORT
+from config import AUTO_PORT, translate
 
 def sender_auto(me, send_to, filename):
     print(f'sending file on auto channel from {me} to {send_to}')
     other = socket()
-
-    other.connect((send_to, AUTO_PORT))
+    
+    other.connect((translate[send_to], AUTO_PORT))
     print(f"auto connection stablished on port:{AUTO_PORT}")
     
     send_protocol(other, IS_FILE, filename)
@@ -20,7 +20,7 @@ def receiver_auto(me, who='unkown'):
 
     print(f"auto connection waiting on port:{AUTO_PORT}")
     other, address = server.accept()
-    print(f"auto connection stablished with address:{address}!")
+    print(f"auto connection stablished with address:{address}")
 
     read_protocol(other)
 
